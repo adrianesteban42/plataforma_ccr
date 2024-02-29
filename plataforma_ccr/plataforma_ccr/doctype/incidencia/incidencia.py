15d917cac6af29573d130599067dce840d10ed50
adrianesteban42 <adrianesteban42@gmail.com>
2024-02-29 17:52:30 +0100

    agragando funcion validar tipo

--- plataforma_ccr/plataforma_ccr/doctype/incidencia/incidencia.py
+++ plataforma_ccr/plataforma_ccr/doctype/incidencia/incidencia.py
@@ -1,9 +1,21 @@
 # Copyright (c) 2024, Adrian Martinez and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.website.website_generator import WebsiteGenerator
 
 
 class Incidencia(WebsiteGenerator):
-	pass
+
+	def validate(self):
+		# Obtener el valor del campo 'tipo'
+		tipo = self.tipo_incidencia
+
+		# Verificar si el tipo es 'Bien'
+		if tipo == 'Bien':
+			# Si el tipo es 'Bien', hacer que el campo de imagen sea obligatorio
+			if not self.img_video_incidencia:
+				frappe.throw("Por favor, adjunte una imagen para la incidencia de tipo 'Bien'")
+		elif tipo == 'Servicio':
+			# Si el tipo es 'Servicio', asegurarse de que el campo de imagen no sea obligatorio
+			self.img_video_incidencia = None
