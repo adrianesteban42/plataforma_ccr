cafc0f82a9692f6626e6001a0cc90d6703a54b19
adrianesteban42 <adrianesteban42@gmail.com>
2024-03-05 11:44:52 +0100

    agregando funcion actualizar estado al enviar

--- plataforma_ccr/plataforma_ccr/doctype/incidencia/incidencia.py
+++ plataforma_ccr/plataforma_ccr/doctype/incidencia/incidencia.py
@@ -9,20 +9,5 @@ class Incidencia(WebsiteGenerator):
 	def before_submit(self):
 		
 		self.fecha_incidencia = frappe.utils.now_datetime()
-	# def before_insert(self):
-    #     # Verificar si la geolocalización no está establecida
-    #     if not self.localizacion_incidencia:
-    #         # Obtener la geolocalización actual del navegador
-    #         geolocation = self.get_current_geolocation()
-    #         if geolocation:
-    #             # Establecer la geolocalización en el campo correspondiente
-    #             self.localizacion_incidencia = geolocation
-
-    # def get_current_geolocation(self):
-    #     try:
-    #         # codigo geolocalizacion
-	# 		latitud = 37.7749
-    #         longitud = -122.4194
-    #         return f'{latitud},{longitud}'
-    #     except Exception as e:
-    #         frappe.log_error(f'Error al obtener geolocalización: {str(e)}', 'Incidencia')
+		if self.estado_incidencia == "Borrador":
+			self.estado_incidencia = "Enviado"
