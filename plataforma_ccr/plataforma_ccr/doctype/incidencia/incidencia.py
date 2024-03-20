6541990a084d4d49244a70c8e6259e7d7ccecc08
magoluaces <magoluaces@gmail.com>
2024-03-20 23:32:11 +0100

    empezando v2.0.0

--- plataforma_ccr/plataforma_ccr/doctype/incidencia/incidencia.py
+++ plataforma_ccr/plataforma_ccr/doctype/incidencia/incidencia.py
@@ -1,10 +1,7 @@
-# Copyright (c) 2024, Adrian Martinez and contributors
-# For license information, please see license.txt
-
 import frappe
 from frappe.website.website_generator import WebsiteGenerator
 
+
 class Incidencia(WebsiteGenerator):
-    def before_submit(self):
-        self.estado_incidencia = 'Enviado'
-	
+	def validate(self):
+		self.estado_incidencia = 'Enviado'
