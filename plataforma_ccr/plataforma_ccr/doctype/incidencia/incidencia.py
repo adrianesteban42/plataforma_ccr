134e3003b5b9acffd87258e10e7b36a44106a524
adrianesteban42 <adrianesteban42@gmail.com>
2024-06-11 18:00:31 +0200

    agregando doctype elemento

--- plataforma_ccr/plataforma_ccr/doctype/incidencia/incidencia.py
+++ plataforma_ccr/plataforma_ccr/doctype/incidencia/incidencia.py
@@ -2,12 +2,13 @@
 # For license information, please see license.txt
 
 import frappe
+import datetime
 from frappe.website.website_generator import WebsiteGenerator
 
 class Incidencia(WebsiteGenerator):
 
 	def before_save(self):
 		
-		# self.fecha_incidencia = frappe.utils.now_datetime()
+		self.fecha_incidencia = datetime.date.today().strftime("%Y-%m-%d")
 		if self.estado_incidencia == "Borrador":
 			self.estado_incidencia = "Enviado"
